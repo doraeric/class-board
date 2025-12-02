783199f82de243827f2f954cf9c92de9cc028d3b
doraeric <benson.doraemon@gmail.com>
2025-12-03 00:53:04 +0800

    chore: add tanstack router dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    // https://github.com/tailwindlabs/tailwindcss/issues/18802
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }) as any,
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
